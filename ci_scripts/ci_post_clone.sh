b765b94513ba92766a7fd60fe23198fa804dea9a
snowballs24 <benino7@hotmail.com>
2026-07-22 08:26:05 +1000

    Fix Xcode Cloud Capacitor resource archive

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,3 +5,7 @@ cd "$CI_PRIMARY_REPOSITORY_PATH"
 npm ci
 npm run build:web
 npx cap sync ios
+
+test -f ios/App/App/capacitor.config.json
+test -f ios/App/App/config.xml
+test -f ios/App/App/public/index.html
